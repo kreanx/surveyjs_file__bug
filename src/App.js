5105b42a0fd56d21650b16c15e011003d314cd63
Путинцев Никита <nputintsev@neoflex.ru>
2024-02-13 15:42:09 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import { Survey } from 'survey-react-ui';
+import { Model } from "survey-core";
+import "survey-core/defaultV2.min.css";
+
+const surveyJson = {
+  "logoPosition": "right",
+  "pages": [
+    {
+      "name": "page1",
+      "elements": [
+        {
+          "type": "file",
+          "name": "question3"
+        }
+      ]
+    }
+  ]
+};
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const survey = new Model(surveyJson);
+
+  return <Survey model={survey} />;
 }
 
 export default App;
